c6a2c772e28e7d15d31b0679f68c54827ea0b364
argen7um <argen7um.music@yandex.ru>
2023-11-09 17:53:12 +0700

    [FIX] fix some errors

--- routers/message.py
+++ routers/message.py
@@ -9,24 +9,19 @@ message_router = APIRouter()
 @message_router.post('/api/v1/message')
 async def create_message(text: str, conversation_id:int, 
                          user_id: str = Depends(verificator.verify_jwt)):
-    if user_id:
-        user = (await user_getter.get_by_id(int(user_id)))[0][0]
+        try:
+            user = (await user_getter.get_by_id(int(user_id)))[0][0]
+        except Exception: 
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+
         message = Message(text = text, 
                           conversation_id=conversation_id, 
                           user_id=user.id)
         await message_creator.create(message)
         return message
     
-@message_router.get('/api/v1/message/{limit}')
-async def get_messages_by_username_and_id_from_db(id: int, limit: int=10, 
-                                                  user_id: str = Depends(verificator.verify_jwt)):
-    if user_id:
-        user = (await user_getter.get_by_id(int(user_id)))[0]
-        messages = await message_getter.get_by_user_and_id(user, id)
-        return messages[:limit]
-    
 @message_router.get('/api/v1/conversation_messages/')
-async def get_messages_by_conversation_id(conversation_id: int, limit: int=10, 
+async def get_messages_by_conversation_id(conversation_id: int, limit: int = 0, 
                                           user_id: str = Depends(verificator.verify_jwt)):
     if user_id:
         user = (await user_getter.get_by_id(int(user_id)))[0]
--- routers/user.py
+++ routers/user.py
@@ -1,14 +1,17 @@
 from fastapi import APIRouter, Depends, HTTPException, status
 from utils import user_creator
 from models.models import User, ConversationModel
-
+from asyncpg.exceptions import UniqueViolationError
 
 user_router = APIRouter()
 
 @user_router.post('/api/v1/register')
 async def resister_user(username: str, password: str):
     user = User(username=username, password=password)
-    db_user = await user_creator.create(user)
-    print(db_user)
+    try:
+        db_user = await user_creator.create(user)
+    except UniqueViolationError:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, 
+                            detail='The username or password are occupied')
     return db_user
 
